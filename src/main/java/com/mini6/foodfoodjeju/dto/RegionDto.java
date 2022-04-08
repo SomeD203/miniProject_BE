94c266aa25cb23eefc3126d737b266d092b91493
whyyougd <holygd73@naver.com>
2022-04-09 00:07:28 +0900

    회원가입 및 오픈api 기능_1

---
+++ src/main/java/com/mini6/foodfoodjeju/dto/RegionDto.java
@@ -0,0 +1,6 @@
+package com.mini6.foodfoodjeju.dto;
+
+public class RegionDto {
+    private String dong;
+    private Long regionId;
+}
---
+++ src/main/java/com/mini6/foodfoodjeju/dto/StoreInfoDto.java
@@ -0,0 +1,14 @@
+package com.mini6.foodfoodjeju.dto;
+
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class StoreInfoDto {
+    private String title;
+    private String address;
+    private String phoneno;
+    private Map<String, String> region2cd;
+
+}
