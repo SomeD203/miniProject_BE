22f4fbcaa84d7960a90f243e8695b1c5bcd4dce4
whyyougd <holygd73@naver.com>
2022-04-14 16:34:33 +0900

    Detail Controller에서 heart, comment Cnt 추가

--- src/main/java/com/mini6/foodfoodjeju/controller/DetailController.java
+++ src/main/java/com/mini6/foodfoodjeju/controller/DetailController.java
@@ -1,11 +1,14 @@
 package com.mini6.foodfoodjeju.controller;
 
 import com.mini6.foodfoodjeju.model.OpenApi;
+import com.mini6.foodfoodjeju.model.TestStore;
 import com.mini6.foodfoodjeju.repository.CommentRepository;
 import com.mini6.foodfoodjeju.repository.OpenApiRepository;
+import com.mini6.foodfoodjeju.security.UserDetailsImpl;
 import com.mini6.foodfoodjeju.service.DetailService;
 import com.mini6.foodfoodjeju.service.StoreInfoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,9 +29,18 @@ public class DetailController {
     }
 
 
-    @GetMapping("/api/main/{storeId}/detail")
-    public OpenApi getStoreDetail(@PathVariable Long storeId) {
+//    @GetMapping("/api/main/{storeId}/detail")
+//    public OpenApi getStoreDetail(@PathVariable Long storeId) {
+//
+//        return detailService.getDetail(storeId);
+//    }
 
-        return detailService.getDetail(storeId);
+    @GetMapping("/api/main/{storeId}/detail")
+    public List<TestStore> testStores(@PathVariable Long storeId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if(userDetails == null){
+            return detailService.getStoresDetail(storeId,null);
+        }
+        return detailService.getStoresDetail(storeId, userDetails);
     }
+
 }
--- src/main/java/com/mini6/foodfoodjeju/service/DetailService.java
+++ src/main/java/com/mini6/foodfoodjeju/service/DetailService.java
@@ -1,28 +1,52 @@
 package com.mini6.foodfoodjeju.service;
 
 import com.mini6.foodfoodjeju.dto.OpenApiDto;
+import com.mini6.foodfoodjeju.model.Heart;
+import com.mini6.foodfoodjeju.model.Joayo;
 import com.mini6.foodfoodjeju.model.OpenApi;
-import com.mini6.foodfoodjeju.repository.DetailRepository;
-import com.mini6.foodfoodjeju.repository.JoayoRepository;
-import com.mini6.foodfoodjeju.repository.OpenApiRepository;
+import com.mini6.foodfoodjeju.model.TestStore;
+import com.mini6.foodfoodjeju.repository.*;
+import com.mini6.foodfoodjeju.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DetailService {
     private final OpenApiRepository openApiRepository;
-    private final DetailRepository detailRepository;
-    private final JoayoRepository joayoRepository;
+    private final CommentRepository commentRepository;
+    private final HeartRepository heartRepository;
 
+//    //
+//    @Transactional
+//    public OpenApi getDetail(Long storeId){
+//
+//        return openApiRepository.findById(storeId).orElse(null);
+//    }
 
+    // Detail페이지에 openapi, 좋아요 개수, 코멘트 개수, 좋아요 상태)
     @Transactional
-    public OpenApi getDetail(Long storeId){
+    public List<TestStore> getStoresDetail(Long storeId, UserDetailsImpl userDetails) {
+        List<TestStore> testStores = new ArrayList<>();
 
-        return openApiRepository.findById(storeId).orElse(null);
+        OpenApi openApi = openApiRepository.findById(storeId).orElse(null);
+        List<Heart> hearts = heartRepository.findByStoreId(openApi.getOpenApiId());
+        boolean heartState = false;
+        if(!(userDetails == null)){
+            for (Heart heart : hearts){
+                if(heart.getUserName().equals(userDetails.getUsername())){
+                    heartState = true;
+                }
+            }
+        }
+        int commentCnt = commentRepository.findAllByStoreId(openApi.getOpenApiId()).size();
+        TestStore testStore = new TestStore(openApi, hearts.size(), commentCnt, heartState);
+        testStores.add(testStore);
 
+        return testStores;
     }
 }
